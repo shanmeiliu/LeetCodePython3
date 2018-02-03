d96fa1e63cebda2378caa309166b4c1057c827cb
Shanmei Liu <sl8145@mun.ca>
2018-02-02 23:04:15 -0330

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -14,3 +14,26 @@ class Solution:
                 return [lookup[target - num], i]
             lookup[num] = i
         
+"""
+Run Code Result:
+
+Your input
+
+[3,2,4]
+6
+Your stdout
+
+0 3
+{}
+1 2
+{3: 0}
+2 4
+{2: 1, 3: 0}
+{2: 1, 3: 0}
+Your answer
+
+[1,2]
+Expected answer
+
+[1,2]
+"""
